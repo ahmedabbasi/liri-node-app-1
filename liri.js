3a51f53c7b54c871189f4418faa8bfae4c8a3cd9
Oscar Villalta <bagglerock@gmail.com>
2018-04-03 14:09:55 -0400

    dotenv works now and skeleton made

--- liri.js
+++ liri.js
@@ -1,6 +1,25 @@
 require("dotenv").config();
+var keys = require("./keys");
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
 
-console.log(spotify);
+var spotifyId = keys.spotify.id;
+var spotifySecret = keys.spotify.secret;
+
+var twitterKey = keys.twitter.consumer_key;
+var twitterSecret = keys.twitter.consumer_secret;
+var twitterAccKey = keys.twitter.access_token_key;
+var twitterAccSecret = keys.twitter.access_token_secret;
+
+var arg = process.argv.slice(2);
+
+if(arg[0] === "my-tweets"){
+    console.log("use twitter");
+}
+
+if(arg[0] === "spotify-this-song"){
+    console.log("spotify");
+}
+
+if(arg[0] ==="movie-this"){
+    console.log("omdb it");
+}

c4db9f286791782738a332a8897ab971545a6b41
Oscar Villalta <bagglerock@gmail.com>
2018-04-03 14:53:31 -0400

    just psuedocoded it kind of

--- liri.js
+++ liri.js
@@ -13,13 +13,17 @@ var twitterAccSecret = keys.twitter.access_token_secret;
 var arg = process.argv.slice(2);
 
 if(arg[0] === "my-tweets"){
+    //show my tweets
     console.log("use twitter");
 }
 
 if(arg[0] === "spotify-this-song"){
+    var song = arg.slice(1);
+    //spotify song
     console.log("spotify");
 }
 
 if(arg[0] ==="movie-this"){
+    var song = arg.slice(1);
     console.log("omdb it");
 }

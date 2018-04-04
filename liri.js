a8d9805e1194ab46762777e8b5fe3dfa2806b1d3
Oscar Villalta <bagglerock@gmail.com>
2018-04-03 22:54:40 -0400

    have twitter and the spotify apis working but still need to clean the response

--- liri.js
+++ liri.js
@@ -1,29 +1,55 @@
 require("dotenv").config();
 var keys = require("./keys");
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
 
+var client = new Twitter({
+  consumer_key: keys.twitter.consumer_key,
+  consumer_secret: keys.twitter.consumer_secret,
+  access_token_key: keys.twitter.access_token_key,
+  access_token_secret: keys.twitter.access_token_secret
+});
 
-var spotifyId = keys.spotify.id;
-var spotifySecret = keys.spotify.secret;
-
-var twitterKey = keys.twitter.consumer_key;
-var twitterSecret = keys.twitter.consumer_secret;
-var twitterAccKey = keys.twitter.access_token_key;
-var twitterAccSecret = keys.twitter.access_token_secret;
+var spotify = new Spotify({
+  id: keys.spotify.id,
+  secret: keys.spotify.secret
+});
 
 var arg = process.argv.slice(2);
 
-if(arg[0] === "my-tweets"){
-    //show my tweets
-    console.log("use twitter");
+if (arg[0] === "my-tweets") {
+  var params = {
+    screen_name: "oscar_bootcamp"
+  };
+
+  client.get("statuses/user_timeline", params, function(
+    error,
+    tweets,
+    response
+  ) {
+    if (!error) {
+      console.log(tweets);
+    } else {
+      console.log(error);
+    }
+  });
 }
 
-if(arg[0] === "spotify-this-song"){
-    var song = arg.slice(1);
-    //spotify song
-    console.log("spotify");
+if (arg[0] === "spotify-this-song") {
+  var song = arg.slice(1);
+  spotify.search({ type: "track", query: "All the Small Things" }, function(
+    err,
+    data
+  ) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
+
+    console.log(data);
+  });
 }
 
-if(arg[0] ==="movie-this"){
-    var song = arg.slice(1);
-    console.log("omdb it");
+if (arg[0] === "movie-this") {
+  var song = arg.slice(1);
+  console.log("omdb it");
 }

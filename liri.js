f4137d1191e26fb559f8ceb13fffa54407a174bd
Oscar Villalta <bagglerock@gmail.com>
2018-04-09 18:29:32 -0400

    bonus done

--- liri.js
+++ liri.js
@@ -7,6 +7,10 @@ var Spotify = require("node-spotify-api");
 var request = require("request");
 var fs = require("fs");
 
+var separatorThick = "========================" + "\n";
+var separatorThin = "------------------------" + "\n";
+
+
 //  Set up the keys
 var client = new Twitter({
   consumer_key: keys.twitter.consumer_key,
@@ -66,20 +70,24 @@ function getTweets() {
     response
   ) {
     if (!error) {
-      console.log("========================");
-      console.log("These are the last 20 tweets!.");
-      console.log("========================");
+
+      var userResponse = [];
+
+      userResponse.push(separatorThick);
       for (var i = 0; i < tweets.length; i++) {
-        console.log("------------------------");
-        console.log(
-          tweets[i].user.name + " created this tweet on " + tweets[i].created_at
-        );
-        console.log("Tweet: " + tweets[i].text);
+        var nameLine = tweets[i].user.name + " created this tweet on " + tweets[i].created_at + "\n";
+        var tweetLine = "Tweet: " + tweets[i].text + "\n";
+
+        userResponse.push(separatorThin);
+        userResponse.push(nameLine);
+        userResponse.push(tweetLine);
+        userResponse.push(separatorThin);
       }
-      console.log("------------------------");
-      console.log("========================");
-      console.log("These are the last 20 tweets!.");
-      console.log("========================");
+      userResponse.push(separatorThick);
+
+      appendLog(userResponse);
+      console.log(userResponse);
+
     } else {
       console.log("Error occurred: " + error);
     }
@@ -97,15 +105,28 @@ function spotifySong(song) {
       console.log("Error occurred: " + err);
     }
     var result = data.tracks.items;
+
     result.forEach(function(element) {
-      console.log("=============");
-      console.log("Artist Name: " + element.artists[0].name);
-      console.log("Song Name: " + element.name);
-      console.log("Album: " + element.album.name);
-      console.log("Release Date: " + element.album.release_date);
-      console.log("Preview URL: " + previewUrl + element.id);
+      var userResponse = [];
+
+      var artistLine = "Artist Name: " + element.artists[0].name + "\n";
+      var songLine = "Song Name: " + element.name + "\n";
+      var albumLine = "Album: " + element.album.name + "\n";
+      var releaseLine = "Release Date: " + element.album.release_date + "\n";
+      var previewLine = "Preview URL: " + previewUrl + element.id + "\n";
+
+      userResponse.push(separatorThick);
+      userResponse.push(artistLine);
+      userResponse.push(songLine);
+      userResponse.push(albumLine);
+      userResponse.push(releaseLine);
+      userResponse.push(previewLine);
+      userResponse.push(separatorThick);
+
+      appendLog(userResponse);
+      console.log(userResponse);
+
     });
-    console.log("=============");
   });
 }
 
@@ -115,29 +136,56 @@ function findMovie(movie) {
     "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy",
     function(error, response, body) {
       if (!error && response.statusCode === 200) {
+        var userResponse = [];
         var result = JSON.parse(body);
         var ratings = result.Ratings;
         var rtRating = "not rated";
+        var titleLine = "Title: " + result.Title;
+        var yearLine = "Year: " + result.Year;
+        var imdbLine = "IMDB Rating: " + result.imdbRating;
+        var rottenRating = "Rotten Tomatoes Rating: " + rtRating;
+        var countryLine = "Country(s): " + result.Country;
+        var languageLine = "Language(s): " + result.Language;
+        var plotLine = "Plot: " + result.Plot;
+        var actorLine = "Actors: " + result.Actors;
         for (var key in ratings) {
           var obj = ratings[key];
           if (obj.Source === "Rotten Tomatoes") {
             rtRating = obj.Value;
           }
         }
-        console.log("=============");
-        console.log("Title: " + result.Title);
-        console.log("Year: " + result.Year);
-        console.log("IMDB Rating: " + result.imdbRating);
-        console.log("Rotten Tomatoes Rating: " + rtRating);
-        console.log("Country(s): " + result.Country);
-        console.log("Language(s): " + result.Language);
-        console.log("Plot: " + result.Plot);
-        console.log("Actors: " + result.Actors);
-        console.log("=============");
+
+        userResponse.push(separatorThick);
+        userResponse.push(titleLine);
+        userResponse.push(yearLine);
+        userResponse.push(imdbLine);
+        userResponse.push(rottenRating);
+        userResponse.push(countryLine);
+        userResponse.push(languageLine);
+        userResponse.push(plotLine);
+        userResponse.push(actorLine);
+        userResponse.push(separatorThick);
+
+        for (var i = 0; i<userResponse.length; i++){
+          appendLog(userResponse[i])
+          console.log(userResponse[i]);
+
+        }
       }
+    });
+}
+
+
+function appendLog(response){
+  fs.appendFile("log.txt", response + "\n" , function(err) {
+    if (err) {
+      return console.log(err);
     }
-  );
+  });
 }
 
 //  Start the show
 getCommand(arg);
+
+
+ // have to clean up something.. there are commas and there are \n's on each line
